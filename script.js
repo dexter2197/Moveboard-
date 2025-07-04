820acf404a287355246c24df86611cc9c138236f
dexter2197 <derrickleiv@gmail.com>
2025-07-04 20:25:55 +0300

    Add files via upload
    Delete all old files

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function confirmDownload() {
-    return confirm("Do you want to download this movie?");
+  return confirm("Do you want to download this movie?");
 }
+
+document.getElementById("searchInput").addEventListener("input", function () {
+  const query = this.value.toLowerCase();
+  document.querySelectorAll(".movie").forEach((movie) => {
+    const title = movie.getAttribute("data-title").toLowerCase();
+    movie.style.display = title.includes(query) ? "block" : "none";
+  });
+});
